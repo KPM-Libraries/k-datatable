49f7872319f1b503a04b247e827ad890ba77e4d5
abbacikhaled <abbacikhaled@gmail.com>
2021-11-05 18:51:38 +0100

    add: k datatable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-interface DataTable {
-  addData: boolean,
-  type?: string,
-  style?: string,
-  header: Array<{ name: string, type: string, order: boolean, filter: boolean }>,
-  footer?: Array<string>,
-  items: Array<Array<{ value: any, style?: string, src?: string, width?: string, height?: string, condition?: string }>>
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,7 +8,7 @@ interface DataTable {
 
 export class AppComponent {
 
-  data: DataTable = {
+  data = {
     addData: true,
     header: [
       {
@@ -51,163 +44,73 @@ export class AppComponent {
     ],
     footer: ["Footer 1", "Footer 2", "Footer 3", "Footer 4"],
     items: [
-      [
-        {
-          value: "Cel 12",
-          condition: "fields[1].value === 5"
-        },
-        {
-          value: 5,
-          style: "k-bg-danger"
-        },
-        {
-          value: ["rtas", "World"]
-        },
-        {
-          value: new Date('4/7/2025').toLocaleDateString()
-        },
-        {
-          value: [
-            {
-              style: "k-bg-fuchsia",
-              title: "Modifier",
-              event: "update"
-            }
-          ]
-        }
-      ],
-      [
-        {
-          value: "Cel 5"
-        },
-        {
-          value: 8
-        },
-        {
-          value: ["aello"]
-        },
-        {
-          value: new Date('5/7/2019').toLocaleDateString()
-        }
-      ],
-      [
-        {
-          value: "Cel 1"
-        },
-        {
-          value: 1
-        },
-        {
-          value: ["Hello", "World"]
-        },
-        {
-          value: new Date().toLocaleDateString()
-        }
-      ]
+      {
+        id: 1,
+        data: [
+          {
+            value: "Cel 12",
+            condition: "fields[1].value === 5"
+          },
+          {
+            value: 5,
+            style: "k-bg-danger"
+          },
+          {
+            value: ["rtas", "World"]
+          },
+          {
+            value: new Date('4/7/2025').toLocaleDateString()
+          },
+          {
+            value: [
+              {
+                style: "k-bg-fuchsia",
+                title: "Modifier",
+                event: "update"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        id: 2,
+        data: [
+          {
+            value: "Cel 5"
+          },
+          {
+            value: 8
+          },
+          {
+            value: ["aello"]
+          },
+          {
+            value: new Date('5/7/2019').toLocaleDateString()
+          }
+        ]
+      },
+      {
+        id: 3,
+        data: [
+          {
+            value: "Cel 1"
+          },
+          {
+            value: 1
+          },
+          {
+            value: ["Hello", "World"]
+          },
+          {
+            value: new Date().toLocaleDateString()
+          }
+        ]
+      }
     ]
   }
 
-  filterTable = new Array(this.data?.header?.length)
-
-  startedItems = [...this.data.items]
-
-  orderStatus = {
-    index: -1,
-    type: '',
-    ascOrDescTable: new Array<number>()
+  event(event: any) {
+    console.log(event)
   }
 
-  pagination = {
-    elementsPerPage: 2,
-    current: 1,
-    total: Math.ceil(this.data.items.length / 2)
-  }
-
-
-  //filter part
-  filter(): void {
-    this.data.items = [...this.startedItems]
-    if (this.orderStatus.index != -1)
-      this.order(this.orderStatus.index, this.orderStatus.type, true)
-
-    let i = 0
-    while (i < this.data.items.length)
-      if (!this.filterAccepted(this.data.items[i]))
-        this.data.items.splice(i, 1)
-      else
-        i++
-    this.setPagination()
-  }
-
-  filterAccepted(item: any): Boolean {
-    for (let i = 0; i < this.filterTable.length; i++)
-      if (this.filterTable[i] != null && this.filterTable[i] != '') {
-        switch (this.data.header[i]?.type) {
-          case 'text':
-          case 'bold':
-          case 'badge':
-            if (!item[i]?.value.toLowerCase().includes(this.filterTable[i].toLowerCase()))
-              return false
-            break
-          case 'list':
-            if (!this.filterInsideList(item[i]?.value, this.filterTable[i].toLowerCase()))
-              return false
-            break
-          case 'image':
-            if (!item[i]?.value?.src.includes(this.filterTable[i]))
-              return false
-            break
-        }
-      }
-    return true
-  }
-
-  filterInsideList(list: Array<any>, text: string): Boolean {
-    for (let item of list)
-      if (item.toLowerCase().includes(text))
-        return true
-    return false
-  }
-
-  order(index: number, type: string, force?: boolean): void {
-    if (index != this.orderStatus.index || type != this.orderStatus.type || force) {
-      this.orderStatus.index = index
-      this.orderStatus.type = type
-      this.orderStatus.ascOrDescTable = type == 'ASC' ? [-1, 1] : type == 'DESC' ? [1, -1] : []
-      if (this.orderStatus.ascOrDescTable.length > 0)
-        this.data.items = this.data?.items.sort((elt1: any, elt2: any): number => {
-          console.log(55555)
-          if (typeof elt1[index]?.value != typeof elt2[index]?.value)
-            return -1
-          switch (this.data.header[index]?.type) {
-            case 'date':
-              return (new Date(elt1[index]?.value).getTime() - new Date(elt2[index]?.value).getTime()) * this.orderStatus.ascOrDescTable[1]
-            case 'list':
-              return elt1[index]?.value.join('').toLowerCase() == elt2[index]?.value.join('').toLowerCase() ? 0 : elt1[index]?.value.join('').toLowerCase() < elt2[index]?.value.join('').toLowerCase() ? this.orderStatus.ascOrDescTable[0] : this.orderStatus.ascOrDescTable[1]
-            default: //all other types
-              return elt1[index]?.value.toString().toLowerCase() == elt2[index]?.value.toString().toLowerCase() ? 0 : elt1[index]?.value.toString().toLowerCase() < elt2[index]?.value.toString().toLowerCase() ? this.orderStatus.ascOrDescTable[0] : this.orderStatus.ascOrDescTable[1]
-          }
-        })
-    } else {
-      this.orderStatus.index = -1
-      this.orderStatus.type = ''
-      this.filter()
-    }
-    this.setPagination()
-  }
-
-  setPagination(elementPerPage?: number): void {
-    let total = Math.ceil(this.data.items.length / 2)
-    this.pagination = {
-      elementsPerPage: elementPerPage ? elementPerPage : this.pagination.elementsPerPage,
-      current: this.pagination.current <= total && this.pagination.current != 0 ? this.pagination.current : total,
-      total: total
-    }
-  }
-
-  evalCondition(condition: string, index: number) {
-    //condition: "fields[0].value === 0"
-    condition = condition.split("fields[").join("this.data.items[index][")
-    return eval(condition)
-  }
 }
--- src/app/components/k-datatable/k-datatable.component.ts
+++ src/app/components/k-datatable/k-datatable.component.ts
@@ -1,25 +1,155 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 interface DataTable {
   addData: boolean,
   type?: string,
   style?: string,
   header: Array<{ name: string, type: string, order: boolean, filter: boolean }>,
   footer?: Array<string>,
-  items: Array<Array<{ value: any, style?: string, src?: string, width?: string, height?: string, condition?: string }>>
+  items: Array<{ id: number, data: Array<{ value: any, style?: string, src?: string, width?: string, height?: string, condition?: string }> }>
 }
 
 @Component({
-  selector: 'app-k-datatable',
+  selector: 'k-datatable',
   templateUrl: './k-datatable.component.html',
   styleUrls: ['./k-datatable.component.scss']
 })
 
 export class KDatatableComponent implements OnInit {
 
+  //IO variables
+  @Input() data: DataTable = {
+    addData: false,
+    header: [],
+    items: []
+  }
+  @Output() event = new EventEmitter()
+
+  //component variables
+  filterTable: any
+  startedItems: any
+  orderStatus: any
+  pagination: any
+
   constructor() { }
 
   ngOnInit(): void {
+    //init filter table with the number of columns length
+    this.filterTable = new Array(this.data?.header?.length)
+
+    //save started data items
+    this.startedItems = [...this.data.items]
+
+    //init current order status
+    this.orderStatus = {
+      index: -1,
+      type: '',
+      ascOrDescTable: new Array<number>()
+    }
+
+    //init pagination
+    this.pagination = {
+      elementsPerPage: 10,
+      current: 1,
+      total: Math.ceil(this.data.items.length / 10)
+    }
+  }
+
+  //component functions
+
+  //emit event
+  emitEvent(event: any) {
+    this.event.emit(event)
+  }
+
+  //filter rows
+  filter(): void {
+    this.data.items = [...this.startedItems]
+    if (this.orderStatus.index != -1)
+      this.order(this.orderStatus.index, this.orderStatus.type, true)
+
+    let i = 0
+    while (i < this.data.items.length)
+      if (!this.filterAccepted(this.data.items[i]))
+        this.data.items.splice(i, 1)
+      else
+        i++
+    this.setPagination()
+  }
+
+  //verify if filter is true in some cel
+  filterAccepted(item: any): Boolean {
+    for (let i = 0; i < this.filterTable.length; i++)
+      if (this.filterTable[i] != null && this.filterTable[i] != '') {
+        switch (this.data.header[i]?.type) {
+          case 'text':
+          case 'bold':
+          case 'badge':
+            if (!item[i]?.value.toLowerCase().includes(this.filterTable[i].toLowerCase()))
+              return false
+            break
+          case 'list':
+            if (!this.filterInsideList(item[i]?.value, this.filterTable[i].toLowerCase()))
+              return false
+            break
+          case 'image':
+            if (!item[i]?.value?.src.includes(this.filterTable[i]))
+              return false
+            break
+        }
+      }
+    return true
+  }
+
+  //verify if filter is true in list
+  filterInsideList(list: Array<any>, text: string): Boolean {
+    for (let item of list)
+      if (item.toLowerCase().includes(text))
+        return true
+    return false
+  }
+
+  //order rows
+  order(index: number, type: string, force?: boolean): void {
+    if (index != this.orderStatus.index || type != this.orderStatus.type || force) {
+      this.orderStatus.index = index
+      this.orderStatus.type = type
+      this.orderStatus.ascOrDescTable = type == 'ASC' ? [-1, 1] : type == 'DESC' ? [1, -1] : []
+      if (this.orderStatus.ascOrDescTable.length > 0)
+        this.data.items = this.data?.items.sort((elt1: any, elt2: any): number => {
+          console.log(55555)
+          if (typeof elt1[index]?.value != typeof elt2[index]?.value)
+            return -1
+          switch (this.data.header[index]?.type) {
+            case 'date':
+              return (new Date(elt1[index]?.value).getTime() - new Date(elt2[index]?.value).getTime()) * this.orderStatus.ascOrDescTable[1]
+            case 'list':
+              return elt1[index]?.value.join('').toLowerCase() == elt2[index]?.value.join('').toLowerCase() ? 0 : elt1[index]?.value.join('').toLowerCase() < elt2[index]?.value.join('').toLowerCase() ? this.orderStatus.ascOrDescTable[0] : this.orderStatus.ascOrDescTable[1]
+            default: //all other types
+              return elt1[index]?.value.toString().toLowerCase() == elt2[index]?.value.toString().toLowerCase() ? 0 : elt1[index]?.value.toString().toLowerCase() < elt2[index]?.value.toString().toLowerCase() ? this.orderStatus.ascOrDescTable[0] : this.orderStatus.ascOrDescTable[1]
+          }
+        })
+    } else {
+      this.orderStatus.index = -1
+      this.orderStatus.type = ''
+      this.filter()
+    }
+    this.setPagination()
+  }
+
+  //set pagination params
+  setPagination(elementPerPage?: number): void {
+    let total = Math.ceil(this.data.items.length / 2)
+    this.pagination = {
+      elementsPerPage: elementPerPage ? elementPerPage : this.pagination.elementsPerPage,
+      current: this.pagination.current <= total && this.pagination.current != 0 ? this.pagination.current : total,
+      total: total
+    }
+  }
+
+  //evaluate if some condition is true
+  evalCondition(condition: string, index: number) {
+    condition = condition.split("fields[").join("this.data.items[index].data[")
+    return eval(condition)
   }
 
 }
